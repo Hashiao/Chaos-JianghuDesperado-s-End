8a611e6b13a299528c4c1d90a81f47f76e44d0ef
9950X3D-5090D\Administrator <894149540@qq.com>
2026-01-31 21:29:04 +0800

    调整画面比例 游戏开始的时候地图雾蒙蒙

--- game/js/plugins.js
+++ game/js/plugins.js
@@ -5,5 +5,6 @@ var $plugins =
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"1600","screenHeight":"900","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"CustomLogo","status":true,"description":"Shows RPG Maker and user logos at the start of the game.","parameters":{"logo1":"","logo1ImageName":"MadeWithMv","logo1Skippable":"true","logo1Coordinate":"","logo1X":"800","logo1Y":"450","logo1Origin":"0.5","logo1Time":"","logo1FadeinFrames":"12","logo1FadeoutFrames":"12","logo1DurationFrames":"120","logo2":"","logo2ImageName":"","logo2Skippable":"true","logo2Coordinate":"","logo2X":"800","logo2Y":"450","logo2Origin":"0.5","logo2Time":"","logo2FadeinFrames":"12","logo2FadeoutFrames":"12","logo2DurationFrames":"120","logo3":"","logo3ImageName":"","logo3Skippable":"true","logo3Coordinate":"","logo3X":"800","logo3Y":"450","logo3Origin":"0.5","logo3Time":"","logo3FadeinFrames":"12","logo3FadeoutFrames":"12","logo3DurationFrames":"120","allowTotalSkip":"true"}},
 {"name":"ChaosTitleScene","status":true,"description":"","parameters":{}},
-{"name":"ChaosGameLayout","status":true,"description":"","parameters":{}}
+{"name":"ChaosGameLayout","status":true,"description":"","parameters":{}},
+{"name":"ChaosGameIntro","status":true,"description":"","parameters":{}}
 ];
