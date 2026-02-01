a6921f0ab83de71acf7ac6c00a2cddae2747af62
9950X3D-5090D\Administrator <894149540@qq.com>
2026-02-01 15:33:46 +0800

    恢复fog功能

--- game/js/plugins.js
+++ game/js/plugins.js
@@ -9,5 +9,6 @@ var $plugins =
 {"name":"ChaosCharacterStats","status":true,"description":"","parameters":{}},
 {"name":"ChaosDialogues","status":true,"description":"","parameters":{}},
 {"name":"ChaosGameLayout","status":true,"description":"","parameters":{}},
-{"name":"ChaosGameIntro","status":true,"description":"","parameters":{}}
+{"name":"ChaosGameIntro","status":true,"description":"","parameters":{}},
+{"name":"ChaosVisionFog","status":true,"description":"","parameters":{}}
 ];
